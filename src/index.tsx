5fd433561480d29acec9028b50e9738bb1779bd5
Rabz92 <rabbiailyas92@gmail.com>
2021-08-24 15:38:25 +0200

    packages update

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { configureStore, persistor } from './redux/store/store';
-import { persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
